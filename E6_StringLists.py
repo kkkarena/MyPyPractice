e8baec99171c6c7e4d59388dbe0e0fd2bf7efff2
Karena <Karena_Chuang@pegatroncorp.com>
2018-04-23 14:24:08 +0800

    E6_StringLists

---
+++ E6_StringLists.py
@@ -0,0 +1,19 @@
+string = input("What's your name?")
+
+s = string[:]
+new_list = []
+
+len = len(s)
+'''
+for idx in range(len):
+	new_list = new_list.append(name_list(len-idx))
+	print(name_list(0))
+'''
+print('Total Length: '+ str(len))	
+print(s)
+
+'''
+string = "example"
+s = string[0:5]
+print(s)
+'''
